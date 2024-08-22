8ca120c765f982bce52b2233dfb96cb99d8393b8
flvsantos15 <flvsantos300@gmail.com>
2024-08-22 20:07:48 -0300

    chore: add update integration product test

---
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,42 @@
+import { Sequelize } from 'sequelize-typescript'
+import Product from '../../../domain/product/entity/product'
+import ProductRepository from '../../../infrastructure/product/repository/sequelize/product.repository'
+import UpdateProductUseCase from './update.product.usecase'
+import ProductModel from '../../../infrastructure/product/repository/sequelize/product.model'
+
+const input = {
+  id: '123',
+  name: 'Product 1',
+  price: 20
+}
+
+describe('Unit test update product', () => {
+  let sequelize: Sequelize
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      sync: { force: true }
+    })
+
+    await sequelize.addModels([ProductModel])
+    await sequelize.sync()
+  })
+
+  afterEach(async () => {
+    await sequelize.close()
+  })
+
+  it('should update a product', async () => {
+    const repository = new ProductRepository()
+    const useCase = new UpdateProductUseCase(repository)
+
+    await repository.create(new Product('123', 'Product 1', 15))
+
+    const output = await useCase.execute(input)
+
+    expect(output).toEqual(input)
+  })
+})
